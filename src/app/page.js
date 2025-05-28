c0777b71f032c5e7f17a59afa2c6268e47795425
Cristian Diaconu <cristian.diaconu0423@gmail.com>
2025-05-28 18:40:21 -0400

    Added leather background for landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,31 @@
+"use client"
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight);
+    };
+
+    if (typeof window !== "undefined") {
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  });
+
   return (
     <div className="min-h-screen bg-white">
-      <p>h</p>
+      <div className="absolute inset-0 pointer-events-none h-screen">
+        <img src="/leather.png" className="w-full h-screen object-cover" width={windowWidth} height={windowHeight}/>
+        
+      </div>
+
+
+      {/* <p>h</p> */}
     </div>
   );
 }
