15b334a5bfe0cbe125870d37d393ed83cbb1b54b
Cristian Diaconu <cristian.diaconu0423@gmail.com>
2025-06-01 20:11:56 -0400

    Added another product and added shopping cart icon to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,8 @@
 "use client";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { ShoppingCart } from 'lucide-react';
+
 export default function Navbar({moved}) {
     const topStyle = "fixed top-0 left-0 w-full z-50 bg-opacity-100"
     const movedStyle = "fixed top-0 left-0 w-full z-50 bg-stone-900 shadow-xl"
@@ -7,9 +10,14 @@ export default function Navbar({moved}) {
     let finalStyle = moved ? movedStyle : topStyle;
 
     const pathname = usePathname()
+    const [hydrated, setHydrated] = useState(false);
     
-    let productsStyle = pathname === "/products" ? "group block p-4 text-gray-950 bg-white font-bold rounded-full border-0 transition duration-300" :
-        "group block p-4 text-white font-bold rounded-sm border-0 hover:text-white transition duration-300 hover:underline"
+    let productsStyle = hydrated && pathname === "/products" ? "group block px-3 py-2 text-gray-950 bg-white font-bold rounded-full border-0 transition duration-300" :
+        "group block px-5 py-2 text-white font-bold rounded-sm border-0 hover:text-white transition duration-300 hover:underline"
+
+    useEffect(() => {
+        setHydrated(true);
+    }, []);
 
     return (
         <nav className={finalStyle}>
@@ -19,15 +27,17 @@ export default function Navbar({moved}) {
                     <img src="/eldilogo.png" width={110} height={110} className="p-3"/>
                 </a>
 
-                <div>
+                <div className="flex flex-row justify-center items-center gap-6">
                     <ul className="font-medium flex flex-row space-x-2 rtl:space-x-reverse">
                         <li className="flex flex-col">
-                            <a className="group block p-4 text-white font-bold rounded-sm border-0 hover:text-white transition duration-300 hover:underline" href="/products"> Products </a>
+                            <a className="group block px-5 py-2 text-white font-bold rounded-sm border-0 hover:text-white transition duration-300 hover:underline" href="/products"> Products </a>
                         </li>
                         <li className="flex flex-col">
-                            <a className="group block p-4 text-white font-bold rounded-sm border-0 hover:text-white transition duration-300 hover:underline"> Contact us </a>
+                            <a className="group block px-5 py-2 text-white font-bold rounded-sm border-0 hover:text-white transition duration-300 hover:underline"> Contact us </a>
                         </li>
                     </ul>
+
+                    <ShoppingCart className="text-white"/>
                 </div>
             </div>
         </nav>
