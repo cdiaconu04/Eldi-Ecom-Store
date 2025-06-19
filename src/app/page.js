78a99b8f8c0429082df0567d46fee81d97b220f9
Cristian Diaconu <cristian.diaconu0423@gmail.com>
2025-06-19 13:00:12 -0400

    Added responsive design for logo and first header

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,7 @@ import { Noto_Serif } from 'next/font/google';
 export default function Home() {
   const [windowWidth, setWindowWidth] = useState(0);
   const [windowHeight, setWindowHeight] = useState(0);
+  const [windowSize, setWindowSize] = useState("");
 
   const popularRef = useRef(null);
   const [isInView, setIsInView] = useState(false);
@@ -34,6 +35,14 @@ export default function Home() {
     addRecents(item)
   }
 
+  function getLogoDimensions() {
+    if (windowSize == "sm") return 220;
+    else if (windowSize == "md") return 220;
+    else if (windowSize == "lg") return 190;
+    else if (windowSize == "xl") return 220;
+    else return 220;
+  }
+
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
@@ -43,10 +52,8 @@ export default function Home() {
           setIsInView(true);
         }
       },
-        
-      // { threshold: 0.11 }
+      
       { threshold: 0.9999 },
-      // { smooth: true }
     );
             
     if (popularRef.current) {
@@ -64,6 +71,13 @@ export default function Home() {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
       setWindowHeight(window.innerHeight);
+
+      if (windowWidth >= 1536) setWindowSize("2xl");
+      else if (windowWidth >= 1280) setWindowSize("xl");
+      else if (windowWidth >= 1024) setWindowSize("lg");
+      else if (windowWidth >= 768) setWindowSize("md");
+      else setWindowSize("sm");
+
     };
 
     handleResize();
@@ -87,16 +101,28 @@ export default function Home() {
           <div className="flex flex-col gap-4 p-8 rounded-lg">
             
             {/* <h1 className="text-white text-9xl font-bold z-10 font-serif">eldi</h1> */}
-            <img src="/eldilogocropped.png" width={220} height={220} className="py-2"/>
+            <img src="/eldilogocropped.png" width={getLogoDimensions()} height={getLogoDimensions()} className="py-2"/>
             
-            <h2 className="text-white text-3xl font-bold z-10 font-serif py-3"> Elegant, Timeless, Leather Goods. </h2>
+            <h2 className="text-white font-bold z-10 font-serif py-3
+              2xl:text-3xl xl:text-3xl lg:text-2xl md:text-xl sm:text-lg"
+              > Elegant, Timeless, Leather Goods. </h2>
             
+            {/* sm:text-sm md:text-md lg:text-md xl:text-md 2xl:text-lg */}
             <Link href="/products" className="w-fit rounded-full">
-              <motion.button className="w-fit bg-stone-800 font-serif font-bold px-5 py-2 hover:bg-stone-950 rounded-full mouse-point cursor-pointer"
+              <motion.button className="w-fit bg-stone-800 font-serif font-bold px-5 py-2 hover:bg-stone-950 rounded-full mouse-point cursor-pointer
+                sm:text-sm md:text-sm lg:text-md xl:text-md 2xl:text-md"
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.97 }}
               > View Products </motion.button>
             </Link>
+
+            {/* <motion.button onClick={() => router.push("/products")} className="w-fit bg-stone-800 font-serif font-bold px-5 py-2 hover:bg-stone-950 rounded-full mouse-point cursor-pointer
+              sm:text-7xl md:text-md lg:text-md xl:text-md 2xl:text-md"
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.97 }}
+            >
+              View Products
+            </motion.button> */}
             
 
           </div>
