48daf6b82d217c2007d84509d4d900ae62d3a39d
Cristian Diaconu <cristian.diaconu0423@gmail.com>
2025-06-28 15:49:34 -0400

    Fixed stripe not working

--- src/app/products/[slug]/page.js
+++ src/app/products/[slug]/page.js
@@ -10,7 +10,6 @@ import { useRecents } from "../../../context/recents";
 import { loadStripe } from '@stripe/stripe-js';
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
-
 import Link from "next/link";
 // import { ToastContainer, ToastContentProps, toast } from 'react-toastify';
 import toast, { Toaster } from 'react-hot-toast';
