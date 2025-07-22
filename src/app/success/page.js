5a0ed1bf1105479aee4aae074ae0db23683496dd
Cristian Diaconu <cristian.diaconu0423@gmail.com>
2025-07-22 16:13:34 -0400

    Fixed success and cancel page responsive designs

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -12,11 +12,13 @@ export default function SuccessPage() {
         <div className="w-full bg-stone-200">
             <div className="max-w-screen-xl mx-auto min-h-screen py-30 flex flex-col gap-5 items-center justify-center relative overflow-hidden">
                 <h1 className="font-bold font-serif text-gray-950
-                    2xl:text-3xl xl:text-2xl lg:text-2xl md:text-xl sm:text-lg text-base 
+                    2xl:text-3xl xl:text-3xl lg:text-2xl md:text-2xl sm:text-xl text-lg
                 "> Thank you for shopping at Eldi! </h1>
 
                 <Link href="/products">
-                    <motion.button className="flex flex-row items-center justify-center pr-5 pl-2 py-2 bg-stone-800 rounded-full hover:bg-stone-950 cursor-pointer"
+                    <motion.button className="flex flex-row items-center justify-center pr-5 pl-2 bg-stone-800 rounded-full hover:bg-stone-950 cursor-pointer
+                        xl:py-2 md:py-1 py-0.5
+                    "
                         whileHover={{ x: -10, scale: 1.02 }}
                         whileTap={{ scale: 0.97 }}
                     >   
