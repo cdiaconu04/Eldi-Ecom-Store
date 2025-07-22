d1d593e80176e3f9c505860ca1075e56408962e2
Cristian Diaconu <cristian.diaconu0423@gmail.com>
2025-07-22 16:10:03 -0400

    Added responsive design for success and cancel pages

--- src/app/cancel/page.js
+++ src/app/cancel/page.js
@@ -11,7 +11,9 @@ export default function CancelPage() {
         <Navbar moved={true} />
         <div className="w-full bg-stone-200">
             <div className="max-w-screen-xl mx-auto min-h-screen py-30 flex flex-col gap-5 items-center justify-center relative overflow-hidden">
-                <h1 className="text-3xl font-bold font-serif text-gray-950"> Payment was canceled.</h1>
+                <h1 className="font-bold font-serif text-gray-950
+                    2xl:text-3xl xl:text-2xl lg:text-2xl md:text-xl sm:text-lg text-base 
+                "> Payment was canceled.</h1>
 
                 <Link href="/products">
                     <motion.button className="flex flex-row items-center justify-center pr-5 pl-2 py-2 bg-stone-800 rounded-full hover:bg-stone-950 cursor-pointer"
@@ -19,7 +21,9 @@ export default function CancelPage() {
                         whileTap={{ scale: 0.97 }}
                     >   
                         <ChevronLeft/>
-                        <p className="font-serif font-bold text-white"> Continue shopping </p>     
+                        <p className="font-serif font-bold text-white
+                            2xl:text-base lg:text-sm text-xs
+                        "> Continue shopping </p>     
                     </motion.button>
                 </Link>
             </div>
